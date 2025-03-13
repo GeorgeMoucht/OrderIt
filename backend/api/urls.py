ee77e094ab916d0fd147a531ca53327025cc6a4d
GeorgeMoucht <giorgosmoucht@gmail.com>
2025-03-13 21:02:22 +0200

    Update .gitignore to exclude unnecessary files

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
-
+    # Authentication
+    path('authentication/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Login
+    path('authentication/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,9 +38,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
     'rest_framework',
+    'rest_framework_simplejwt',
+
+    # Our apps
     'api'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -123,3 +135,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# JWT Configurations
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),  # Token expires in 30 minutes
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),     # Refresh token expires in 1 day
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
