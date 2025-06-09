d28b86d7623cbf6572cba0844d8da6ecbdb24a1b
GeorgeMoucht <giorgosmoucht@gmail.com>
2025-06-09 20:35:12 +0300

    Remove old Android project.

--- frontend/core/src/main/java/com/example/core/auth/TokenStorage.java
+++
@@ -1,8 +0,0 @@
-package com.example.core.auth;
-
-public interface TokenStorage {
-    void saveTokens(String accessToken, String refreshToken);
-    String getAccessToken();
-    String getRefreshToken();
-    void clearTokens();
-}
